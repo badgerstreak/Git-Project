d10fb9a5ec7703e3d3db677b32fea352c8f1eee8
badgerstreak <64753966+badgerstreak@users.noreply.github.com>
2020-09-27 17:38:01 +0100

    Initial Commit

---
+++ Git Project/Git_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_ProjectApp.swift
+//  Git Project
+//
+//  Created by Joe Curran on 27/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct Git_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Git ProjectTests/Git_ProjectTests.swift
@@ -0,0 +1,33 @@
+//
+//  Git_ProjectTests.swift
+//  Git ProjectTests
+//
+//  Created by Joe Curran on 27/09/2020.
+//
+
+import XCTest
+@testable import Git_Project
+
+class Git_ProjectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
